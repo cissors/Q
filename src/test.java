69a178b6de933d2c1974c2c2a0dd0a9d0b982d32
cissors <532146403@qq.com>
2017-04-22 02:42:12 +0800

    b1

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+
+	public static void main(String[] args) {
+		Student s = new Student(); 
+		s.setId(49);
+		s.setName("billy");
+		System.out.println(s.getName() + s.getId() + "!");
+	}
+}
